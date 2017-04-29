5c0a652549a422a2d89766b914a24281a1bc77b4
Akaza Akari (Lee Sung Jae) <akari@hoto.us>
2017-04-29 20:38:04 +0900

    Fix Hard-Coded SNS Button

--- js/sns_button.js
+++ js/sns_button.js
@@ -4,12 +4,10 @@ $(".social-buttons>div").click(function() {
     var text = $(this).attr('data-text');
     switch(click_id) {
         case "facebook":
-            window.open("http://www.facebook.com/sharer/sharer.php?u=" + url,"facebook", "width=800, height=400");
+            window.open(`http://www.facebook.com/sharer/sharer.php?u=${url}`, "facebook", "width=800, height=400");
             break;
         case "twitter":
-            window.open("https://twitter.com/intent/tweet?text=" + text + "&url=" + url + "&via=리브레위키&hashtags=LibreWiki", "facebook", "width=800, height=350");
-            break;
-        case "twitter":
+            window.open(`https://twitter.com/intent/tweet?text=[${text}]%0A&url=${url}&via=${mw.config.values.wgSiteName}&hashtags=${text}`, "twitter", "width=800, height=350");
             break;
         default:
             break;
