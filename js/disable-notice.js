fec91a20f7ba20ab402b6663d287666fc0c188d1
nessunkim <nessunkim@gmail.com>
2017-05-26 19:25:32 +0900

    drop jquery cookie

--- js/disable-notice.js
+++ js/disable-notice.js
@@ -1,6 +1,6 @@
 $(function () {
   'use strict';
   $('.liberty-notice').on('closed.bs.alert', function () {
-    $.cookie('disable-notice', 'yes', { expires: 1, path: '/', domain: 'librewiki.net', secure: false });
+    mw.cookie.set('disable-notice', 'yes', { expires: 3600 * 24, secure: false });
   });
 });
