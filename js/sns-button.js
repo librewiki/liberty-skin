40c10d7acb8924b8af147a1df7c267d3511acf10
Akaza Akari <akari@hoto.us>
2017-06-02 21:31:16 +0900

    Fix ESLint

--- js/sns-button.js
+++ js/sns-button.js
@@ -14,7 +14,7 @@ $('.social-buttons>div').click(function () {
       window.open('https://www.facebook.com/sharer/sharer.php?u=' + url, 'facebook', 'width=800, height=400');
       break;
     case 'twitter':
-      window.open('https://twitter.com/intent/tweet?text='+ mw.config.get('wgSiteName') + ' [[' + text + ']]&url=' + url + '&hashtags=' + mw.config.get('wgSiteName').replace(/ /g,'_'), 'twitter', 'width=800, height=350');
+      window.open('https://twitter.com/intent/tweet?text=' + mw.config.get('wgSiteName') + ' [[' + text + ']]&url=' + url + '&hashtags=' + mw.config.get('wgSiteName').replace(/ /g,'_'), 'twitter', 'width=800, height=350');
       break;
     default:
       break;
