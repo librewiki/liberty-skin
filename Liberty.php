99f6443064329971a0aaf506fd8488dcb7868e6f
Akaza Akari <akari@hoto.us>
2017-06-26 04:18:01 +0900

    LibertyTemplate.php

--- Liberty.php
+++ Liberty.php
@@ -1,9 +1,9 @@
 <?php
-if (function_exists('wfLoadSkin')) {
-    wfLoadSkin('Liberty');
-    // Keep i18n globals so mergeMessageFileList.php doesn't break
-    $wgMessagesDirs['Liberty'] = __DIR__ . '/i18n';
-    return true;
+if ( function_exists( 'wfLoadSkin' ) ) {
+	wfLoadSkin( 'Liberty' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['Liberty'] = __DIR__ . '/i18n';
+	return true;
 } else {
-    die('This version of the Liberty skin requires MediaWiki 1.25+');
+	die( 'This version of the Liberty skin requires MediaWiki 1.25+' );
 }
