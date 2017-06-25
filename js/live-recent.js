01351367d09677fa5003b11f3a9c3503119f63be
Akaza Akari <akari@hoto.us>
2017-06-26 04:48:10 +0900

    Fixed Some JS

--- js/live-recent.js
+++ js/live-recent.js
@@ -1,92 +1,92 @@
-$(function () {
-  'use strict';
-  var documentNamespaces = '0|4|10|12|14|1600';
-  var topicNamespaces = '1|3|5|7|9|11|13|15|2600|1601|1063';
-  var isDocumentTab = true;
-  var limit = $('#live-recent-list')[0].childElementCount;
+$( function () {
+	'use strict';
+	var documentNamespaces = '0|4|10|12|14|1600';
+	var topicNamespaces = '1|3|5|7|9|11|13|15|2600|1601|1063';
+	var isDocumentTab = true;
+	var limit = $( '#live-recent-list' )[ 0 ].childElementCount;
 
-  $('#liberty-recent-tab1').click(function () {
-    $(this).addClass('active');
-    $('#liberty-recent-tab2').removeClass('active');
-    isDocumentTab = true;
-    refreshLiveRecent();
-  });
+	$( '#liberty-recent-tab1' ).click( function () {
+		$( this ).addClass( 'active' );
+		$( '#liberty-recent-tab2' ).removeClass( 'active' );
+		isDocumentTab = true;
+		refreshLiveRecent();
+	} );
 
-  $('#liberty-recent-tab2').click(function () {
-    $(this).addClass('active');
-    $('#liberty-recent-tab1').removeClass('active');
-    isDocumentTab = false;
-    refreshLiveRecent();
-  });
+	$( '#liberty-recent-tab2' ).click( function () {
+		$( this ).addClass( 'active' );
+		$( '#liberty-recent-tab1' ).removeClass( 'active' );
+		isDocumentTab = false;
+		refreshLiveRecent();
+	} );
 
-  function refreshLiveRecent() {
-    if (!$('#live-recent-list').length || $('#live-recent-list').is(':hidden')) {
-      return;
-    }
-    var getParameter = {
-      action: 'query',
-      list: 'recentchanges',
-      rcprop: 'title|timestamp',
-      rcshow: '!bot|!redirect',
-      rctype: 'edit|new',
-      rclimit: limit,
-      format: 'json',
-      rcnamespace: isDocumentTab ? documentNamespaces : topicNamespaces,
-      rctoponly: true
-    };
-    $.ajax({
-      url: mw.util.wikiScript('api'),
-      data: getParameter,
-      xhrFields: {
-        withCredentials: true
-      },
-      dataType: 'json'
-    })
-      .then(function (data) {
-        var recentChanges = data.query.recentchanges;
-        var html = recentChanges.map(function (item) {
-          var time = new Date(item.timestamp);
-          var line = '<li><a class="recent-item" href = "' + (mw.config.get('wgArticlePath')).replace('$1', encodeURIComponent(item.title)) + '" title="' + item.title + '">[' + timeFormat(time) + '] ';
-          var text = '';
-          if (item.type === 'new') {
-            text += '[New]';
-          }
-          text += item.title;
-          if (text.length > 13) {
-            text = text.substr(0, 13);
-            text += '...';
-          }
-          text = text.replace('[New]', '<span class="new">[New] </span>');
-          line += text;
-          line += '</a></li>';
-          return line;
-        }).join('\n');
-        $('#live-recent-list').html(html);
-      }, function () {
-        return;
-      });
-  }
+	function refreshLiveRecent() {
+		if ( !$( '#live-recent-list' ).length || $( '#live-recent-list' ).is( ':hidden' ) ) {
+			return;
+		}
+		var getParameter = {
+			action: 'query',
+			list: 'recentchanges',
+			rcprop: 'title|timestamp',
+			rcshow: '!bot|!redirect',
+			rctype: 'edit|new',
+			rclimit: limit,
+			format: 'json',
+			rcnamespace: isDocumentTab ? documentNamespaces : topicNamespaces,
+			rctoponly: true
+		};
+		$.ajax( {
+			url: mw.util.wikiScript( 'api' ),
+			data: getParameter,
+			xhrFields: {
+				withCredentials: true
+			},
+			dataType: 'json'
+		} )
+			.then( function ( data ) {
+				var recentChanges = data.query.recentchanges;
+				var html = recentChanges.map( function ( item ) {
+					var time = new Date( item.timestamp );
+					var line = '<li><a class="recent-item" href = "' + ( mw.config.get( 'wgArticlePath' ) ).replace( '$1', encodeURIComponent( item.title ) ) + '" title="' + item.title + '">[' + timeFormat( time ) + '] ';
+					var text = '';
+					if ( item.type === 'new' ) {
+						text += '[New]';
+					}
+					text += item.title;
+					if ( text.length > 13 ) {
+						text = text.substr( 0, 13 );
+						text += '...';
+					}
+					text = text.replace( '[New]', '<span class="new">[New] </span>' );
+					line += text;
+					line += '</a></li>';
+					return line;
+				} ).join( '\n' );
+				$( '#live-recent-list' ).html( html );
+			}, function () {
+				return;
+			} );
+	}
 
-  function timeFormat(time) {
-    var aDayAgo = new Date();
-    aDayAgo.setDate(aDayAgo.getDate() - 1);
-    if (time < aDayAgo) {
-      return (time.getFullYear()) + '/' + (time.getMonth() + 1) + '/' + time.getDate();
-    }
-    var hour = time.getHours();
-    var minute = time.getMinutes();
-    var second = time.getSeconds();
-    if (hour < 10) {
-      hour = '0' + hour;
-    }
-    if (minute < 10) {
-      minute = '0' + minute;
-    }
-    if (second < 10) {
-      second = '0' + second;
-    }
-    return hour + ':' + minute + ':' + second;
-  }
+	function timeFormat( time ) {
+		var aDayAgo = new Date();
+		aDayAgo.setDate( aDayAgo.getDate() - 1 );
+		if ( time < aDayAgo ) {
+			return ( time.getFullYear() ) + '/' + ( time.getMonth() + 1 ) + '/' + time.getDate();
+		}
+		var hour = time.getHours();
+		var minute = time.getMinutes();
+		var second = time.getSeconds();
+		if ( hour < 10 ) {
+			hour = '0' + hour;
+		}
+		if ( minute < 10 ) {
+			minute = '0' + minute;
+		}
+		if ( second < 10 ) {
+			second = '0' + second;
+		}
+		return hour + ':' + minute + ':' + second;
+	}
 
   setInterval(refreshLiveRecent, 5 * 60 * 1000);
   refreshLiveRecent();
--- js/sns-button.js
+++ js/sns-button.js
@@ -1,22 +1,22 @@
-$('.social-buttons>div').click(function () {
-  'use strict';
-  var selectedMedia = $(this).attr('class');
-  var host = mw.config.get('wgServer');
-  if (host.startsWith('//')) {
-    host = location.protocol + host;
-  }
-  var url = encodeURIComponent(
-    host + mw.config.get('wgScriptPath') + '/index.php?curid=' + mw.config.get('wgArticleId')
-  );
-  var text = $(this).attr('data-text');
-  switch (selectedMedia) {
-    case 'facebook':
-      window.open('https://www.facebook.com/sharer/sharer.php?u=' + url, 'facebook', 'width=800, height=400');
-      break;
-    case 'twitter':
-      window.open('https://twitter.com/intent/tweet?text=' + mw.config.get('wgSiteName') + ' [[' + text + ']]&url=' + url + '&hashtags=' + mw.config.get('wgSiteName').replace(/ /g, '_'), 'twitter', 'width=800, height=350');
-      break;
-    default:
-      break;
-  }
-});
+$( '.social-buttons>div' ).click( function () {
+	'use strict';
+	var selectedMedia = $( this ).attr( 'class' );
+	var host = mw.config.get( 'wgServer' );
+	if ( host.startsWith( '//' ) ) {
+		host = location.protocol + host;
+	}
+	var url = encodeURIComponent(
+		host + mw.config.get( 'wgScriptPath' ) + '/index.php?curid=' + mw.config.get( 'wgArticleId' )
+	);
+	var text = $( this ).attr( 'data-text' );
+	switch ( selectedMedia ) {
+		case 'facebook':
+			window.open( 'https://www.facebook.com/sharer/sharer.php?u=' + url, 'facebook', 'width=800, height=400' );
+			break;
+		case 'twitter':
+			window.open( 'https://twitter.com/intent/tweet?text=' + mw.config.get( 'wgSiteName' ) + ' [[' + text + ']]&url=' + url + '&hashtags=' + mw.config.get( 'wgSiteName' ).replace( / /g, '_' ), 'twitter', 'width=800, height=350' );
+			break;
+		default:
+			break;
+	}
+} );
