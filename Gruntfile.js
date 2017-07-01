4df33fa987eb9a43b1674e7b7ddaa1ba806352f6
Jack Phoenix <jack@countervandalism.net>
2017-07-01 23:34:53 +0300

    Copy Continuous Integration files from upstream EventLogging extension

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
+
+	grunt.initConfig( {
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: '.'
+		},
+		jscs: {
+			all: [
+				'*.js',
+				'{modules,tests}/**/*.js'
+			]
+		},
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'.stylelintrc',
+				'!node_modules/**'
+			]
+		},
+		stylelint: {
+			all: [
+				'**/*.css',
+				'!node_modules/**'
+			]
+		},
+		banana: {
+			core: 'i18n/core/',
+			jsonschema: 'i18n/jsonschema/'
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint', 'banana', 'stylelint' ] );
+	grunt.registerTask( 'default', 'test' );
+};
