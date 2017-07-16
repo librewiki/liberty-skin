e1e8ee3c8633f75b443f75e6fd768a65b885f633
Hoto Cocoa <cocoa@hoto.us>
2017-07-16 14:54:44 +0900

    Global Setting Wait

--- LibertySpecialPage.alias.php
+++ LibertySpecialPage.alias.php
@@ -5,7 +5,3 @@ $specialPageAliases = array();
 $specialPageAliases['en'] = array(
     'Liberty' => array( 'Liberty' ),
 );
-
-$specialPageAliases['ko'] = array(
-    'Liberty' => array( 'Liberty', '리버티' ),
-);
---
+++ structure/liberty_settings.sql
@@ -0,0 +1,21 @@
+--
+-- Table structure for table `liberty_settings`
+--
+
+CREATE TABLE /*_*/liberty_navbar(
+  date int(10) NOT NULL,
+  modifier int NOT NULL,
+  colorMain text(18) DEFAULT NULL,
+  colorSUb text(18) DEFAULT NULL,
+  colorAutoSub boolean DEFAULT true,
+  sidebarPosition int(2) DEFAULT 2,
+  sidebarContent int(2) DEFAULT 3,
+  navbarLogo text DEFAULT NULL;
+  navbarHideGeneral int(3), 0,
+  navbarHideName boolean DEFAULT false,
+  navbarFastOut boolean DEFAULT false,
+  navbarHideLogin boolean DEFAULT false,
+  loginUseModel boolean DEFAULT true,
+  editToolsHide int(3) DEFAULT 3,
+  metaOpenGraphLogo text DEFAULT NULL
+)/*$wgDBTableOptions*/;
