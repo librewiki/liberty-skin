eb7c69c8aec9c21c6f7c42222227cfa034e22be1
Hoto Cocoa <cocoa@hoto.us>
2017-07-19 10:09:56 +0900

    No custom special page
    Global Setting in LocalSettings.php

--- LibertyHooks.php
+++ LibertyHooks.php
@@ -2,69 +2,61 @@
 // @codingStandardsIgnoreLine
 class LibertyHooks
 {
-    public static function onLoadExtensionSchemaUpdates(DatabaseUpdater $updater)
-    {
-        $updater->addExtensionTable('liberty_settings', __DIR__ . '/.maintenance/liberty_settings.sql');
-        $updater->addExtensionTable('liberty_usersettings', __DIR__ . '/.maintenance/liberty_usersettings.sql');
-        
-        return true;
-    }
-
     public static function onGetPreferences(User $user, array &$preferences)
     {
     $preferences['colorMain'] = array(
         'type' => 'toggle',
-        'label-message' => 'liberty-sp-colorMain',
+        'label-message' => 'prefs-liberty-colorMain',
         'section' => 'rendering/liberty'
     );
 
     $preferences['colorSub'] = array(
         'type' => 'toggle',
-        'label-message' => 'liberty-sp-colorSub',
+        'label-message' => 'prefs-liberty-colorSub',
         'section' => 'rendering/liberty'
     );
 
     $preferences['colorAutoSub'] = array(
         'type' => 'toggle',
-        'label-message' => 'liberty-sp-colorAutoSub',
+        'label-message' => 'prefs-liberty-colorAutoSub',
         'section' => 'rendering/liberty',
         'default' => 'true'
     );
 
     $preferences['sidebarPosition'] = array(
         'type' => 'radio',
-        'label-message' => 'liberty-sp-sidebarPosition',
+        'label-message' => 'prefs-liberty-sidebarPosition',
         'section' => 'rendering/liberty',
         'options' => array(
-            wfMessage('liberty-sp-sidebarPosition_0')->plain() => '0',
-            wfMessage('liberty-sp-sidebarPosition_1')->plain() => '1',
-            wfMessage('liberty-sp-sidebarPosition_2')->plain() => '2'
+            wfMessage('prefs-liberty-sidebarPosition_0')->plain() => '0',
+            wfMessage('prefs-liberty-sidebarPosition_1')->plain() => '1',
+            wfMessage('prefs-liberty-sidebarPosition_2')->plain() => '2'
         ),
         'default' => '0'
     );
 
     $preferences['sidebarContent'] = array(
         'type' => 'radio',
-        'label-message' => 'liberty-sp-sidebarContent',
+        'label-message' => 'prefs-liberty-sidebarContent',
         'section' => 'rendering/liberty',
         'options' => array(
-            wfMessage('liberty-sp-sidebarContent_0')->plain() => '0',
-            wfMessage('liberty-sp-sidebarContent_1')->plain() => '1',
-            wfMessage('liberty-sp-sidebarContent_2')->plain() => '2'
+            wfMessage('prefs-liberty-sidebarContent_0')->plain() => '0',
+            wfMessage('prefs-liberty-sidebarContent_1')->plain() => '1',
+            wfMessage('prefs-liberty-sidebarContent_2')->plain() => '2'
         ),
         'default' => '0'
     );
 
     $preferences['navbarFastOut'] = array(
         'type' => 'toggle',
-        'label-message' => 'liberty-sp-navbarFastOut',
+        'label-message' => 'prefs-liberty-navbarFastOut',
         'section' => 'rendering/liberty',
         'default' => 'true'
     );
 
     $preferences['navbarHideName'] = array(
         'type' => 'toggle',
-        'label-message' => 'liberty-sp-navbarHideName',
+        'label-message' => 'prefs-liberty-navbarHideName',
         'section' => 'rendering/liberty',
         'default' => 'false'
     );
