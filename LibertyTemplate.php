e1138e1f017a55d57305e1b601e6c13fac9ab6f8
Hoto Cocoa <cocoa@hoto.us>
2017-08-30 10:24:05 +0900

    CS

--- LibertyTemplate.php
+++ LibertyTemplate.php
@@ -569,9 +569,9 @@ class LibertyTemplate extends BaseTemplate {
 		global $wgArticlePath;
 		$headings = [];
 		$currentHeading = null;
-		$data = get_object_vars(WikiPage::factory(
+		$data = get_object_vars( WikiPage::factory(
 			Title::newFromText( 'Liberty-Navbar', $defaultNamespace = NS_MEDIAWIKI )
-		)->getContent( Revision::RAW ))['mText'];
+		)->getContent( Revision::RAW ) )['mText'];
 		$lines = explode( "\n", $data );
 
 		foreach ( $lines as $line ) {
