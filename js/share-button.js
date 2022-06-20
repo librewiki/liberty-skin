1b464962022b0423bffc897cf87dbc5bd91e4cee
Jack Phoenix <ashley@uncyclomedia.co>
2022-06-20 14:56:46 +0300

    Coding style tweaks + add missing semicolons for share-button.js

--- js/share-button.js
+++ js/share-button.js
@@ -5,18 +5,18 @@ $( '.tools-share' ).click( function () {
 	if ( host.startsWith( '//' ) ) {
 		host = location.protocol + host;
 	}
-	ns = mw.config.get('wgNamespaceNumber')
+	ns = mw.config.get( 'wgNamespaceNumber' );
 	title = mw.config.get( 'wgTitle' );
 	if ( ns ) {
-		title = mw.config.get( 'wgFormattedNamespaces' )[ns] + ':' + title;
+		title = mw.config.get( 'wgFormattedNamespaces' )[ ns ] + ':' + title;
 	}
-	url = host + mw.config.get( 'wgScriptPath' ) + '/index.php?curid=' + mw.config.get( 'wgArticleId' )
-	navigator.share({
+	url = host + mw.config.get( 'wgScriptPath' ) + '/index.php?curid=' + mw.config.get( 'wgArticleId' );
+	navigator.share( {
 		title: title,
 		text: title + ' - ' + mw.config.get( 'wgSiteName' ),
 		url: url,
 		hashtags: [ mw.config.get( 'wgSiteName' ).replace( / /g, '_' ) ]
-	})
+	} )
 	.catch( function ( error ) {
 		console.error( 'Share API error: ', error );
 	} );
