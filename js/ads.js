4c8d4f63420772ac0adc15a713d1119b4c7b017c
Jack Phoenix <ashley@uncyclomedia.co>
2022-06-20 14:51:26 +0300

    Coding style tweaks for ads.js

--- js/ads.js
+++ js/ads.js
@@ -1,15 +1,15 @@
-$(function () {
-    "use strict";
+$( function () {
+    'use strict';
     var width, rightAds;
-    if ($(".mobile-ads") !== undefined) {
-        width = $(window).width();
-        if (width < 1024) {
-            rightAds = $(".right-ads").html();
-            $(".mobile-ads").html(rightAds);
-            $(".right-ads").remove();
+    if ( $( '.mobile-ads' ) !== undefined ) {
+        width = $( window ).width();
+        if ( width < 1024 ) {
+            rightAds = $( '.right-ads' ).html();
+            $( '.mobile-ads' ).html( rightAds );
+            $( '.right-ads' ).remove();
         }
     }
-    $(".adsbygoogle").each(function () {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
-    });
-});
+    $( '.adsbygoogle' ).each( function () {
+        ( window.adsbygoogle = window.adsbygoogle || [] ).push( {} );
+    } );
+} );
